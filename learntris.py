22a8162e4319e03d820b5871c7829f3f1cf5b747
Nityesh Agarwal <nityeshagarwal@gmail.com>
2018-04-18 16:50:51 +0530

    Test 2: passed
    Used `join()`.

--- learntris.py
+++ learntris.py
@@ -2,6 +2,13 @@
 
 import sys
 
+# MATRIX: a list of strings where each string represents one row
+MATRIX = ['.'*10 for row in range(22)]
+
+def print_matrix():
+    for row in MATRIX:
+        print(' '.join(row))
+
 if __name__ == '__main__':
     choice = raw_input()
     if choice == 'q':
